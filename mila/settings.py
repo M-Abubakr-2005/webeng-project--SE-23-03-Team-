a94bca894cb082a957b1f8edc574c059144cb564
unknown <=>
2026-05-24 20:07:16 +0500

    Fix static files and deployment configuration

--- mila/settings.py
+++ mila/settings.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from pathlib import Path
 
@@ -91,10 +96,9 @@ WSGI_APPLICATION = 'mila.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
