a6c2222b98be40620d98f5939b1cd8d286aca118
unknown <=>
2026-05-26 12:43:29 +0500

    Fixed Railway database setup

--- mila/settings.py
+++ mila/settings.py
@@ -96,8 +96,8 @@ WSGI_APPLICATION = 'mila.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
