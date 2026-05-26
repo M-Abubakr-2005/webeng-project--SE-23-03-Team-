1ac2c37db4abbcf2e65bc1fe8a6bc9d2eb30f960
unknown <=>
2026-05-26 19:37:36 +0500

    Fix Daphne app order

--- mila/settings.py
+++ mila/settings.py
@@ -48,21 +48,22 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'daphne',
+
     'channels',
+
     'apps.accounts',
     'apps.chat',
     'apps.stories',
     'apps.core',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
